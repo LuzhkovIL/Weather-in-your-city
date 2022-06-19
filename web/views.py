def3e9a37668a67eea1838f5e1fab2522658c749
Luzhkov <luzhkov99@inbox.ru>
2022-06-19 20:18:22 +0300

    middle weather_1 and web application

--- web/views.py
+++ web/views.py
@@ -1,4 +1,19 @@
+import requests
 from django.shortcuts import render
 
-def index(requst):
-    return render(requst, 'templates_weather/index.html')
+def index(request):
+    appid = '47c36fb73fccd6ff5ea9cf020919466d'
+    url = 'https://api.openweathermap.org/data/2.5/weather?lat={}&units=metric&appid=' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'inco': res["weather"][0]["icon"]
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'templates_weather/index.html', context)
