3d9b72640eddf6f4d92ae7caa0fd5de8c79af957
Luzhkov <luzhkov99@inbox.ru>
2022-06-22 15:36:19 +0300

    THE END

--- web/views.py
+++ web/views.py
@@ -1,19 +1,31 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = '47c36fb73fccd6ff5ea9cf020919466d'
     url = 'https://api.openweathermap.org/data/2.5/weather?lat={}&units=metric&appid=' + appid
 
-    city = 'London'
-    res = requests.get(url.format(city)).json()
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'inco': res["weather"][0]["icon"]
-    }
+    form = CityForm()
 
-    context = {'info': city_info}
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'inco': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
 
     return render(request, 'templates_weather/index.html', context)
